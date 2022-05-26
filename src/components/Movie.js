b2dc053be523e73dd9de29c2cdeb5fdb66382dd4
yejinim <kristy5725@gmail.com>
2022-05-26 21:53:27 +0900

    8장까지 완성된 소스코드

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
+import {Link} from 'react-router-dom';
 
 function Movie({ title, year, summary, poster, genres }) {
     return (
      <div className="movie">
+         <Link to="/movie-detail" state={{ year, title, summary, poster, genres}}>
             <img src={poster} alt={title} title={title} />
-        <div className="movie_data">
-            <h3 className="movie_title">
+            <div className="movie_data">
+              <h3 className="movie_title">
                 {title}
-            </h3>
+              </h3>
             <h5 className="movie_year">{year}</h5>
             <ul className="movie_genres">
                 {genres.map((genre, index) => {
@@ -18,6 +20,7 @@ function Movie({ title, year, summary, poster, genres }) {
             </ul>
             <p className="movie_summary">{summary.slice(0, 180)}</p>
         </div>
+       </Link>
      </div>
     );
 }
---
+++ src/routes/Detail.js
@@ -0,0 +1,24 @@
+import { getDefaultNormalizer } from '@testing-library/react';
+import React, { useEffect } from 'react';
+import {useLocation} from "react-router-dom"
+
+function Detail() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+     if (location.state === undefined) {
+       navigate("/");
+     } else if (location.state === null) {
+       navigate("/");
+     }
+    });
+    
+    if (location.state) {
+        return <span>{location.state.title}</span>;  
+    } else {
+        return null;
+    }
+}
+
+export default Detail;
